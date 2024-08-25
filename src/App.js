a1f1202a61c2d0495657c3f251904244bd4b8ea1
Aditya Godambe <godambeaditya@gmail.com>
2024-08-25 23:51:52 +0530

    feat:Config React and Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="font-black text-center">Lets create an Webflix</div>
+    </div>
+  );
+}
+
+export default App;
